b6c99e085cba0d66bff58f043274240079b96750
SebastianN97 <sebbe132132@gmail.com>
2020-11-11 13:59:13 +0100

    Creating spec request for comments on articles.

---
+++ spec/requests/api/article/comment_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe "POST /api/articles/:id", type: :request do
+  let(:article) { create(:article) }
+  describe 'successfully' do
+    before do
+      get "/api/articles/#{article.id}"
+    end
+
+    it 'should respond with 200 status' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'returns correct article' do
+      expect(response_json['article']['id']).to eq article.id
+    end
+
+    it 'returns comment on article' do
+      #get the comment from article
+    end
+  end
+end
